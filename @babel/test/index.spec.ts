dd2292c95130c57fa1bc6bdc27d66e34b7649ddc
Jiapeiyao <charlesyjp@qq.com>
2020-09-16 04:26:08 +0800

    Add some more unit test cases

--- @babel/test/index.spec.ts
+++ @babel/test/index.spec.ts
@@ -21,3 +21,13 @@ test("import Button from 'antd/button", () => {
   const result = transformSync("import Button from 'antd/button';", options);
   expect(result?.code).toBe('const Button = antd["button"];');
 });
+
+test("import React from 'react';", () => {
+  const result = transformSync("import React from 'react';", options);
+  expect(result?.code).toBe('');
+});
+
+test("import React, { useState } from 'react';", () => {
+  const result = transformSync("import React, { useState } from 'react';", options);
+  expect(result?.code).toBe('const useState = React.useState;');
+});
